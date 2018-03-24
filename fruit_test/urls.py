b710b633c9a33fc038054d9233764cdb4f197ac6
Lu <cheerous@gmail.com>
2018-03-23 20:44:54 -0400

    init

--- fruit_test/urls.py
+++ fruit_test/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from fruit_test import views
-from django.conf.urls.static import static
-from django.conf import settings
-from django.contrib import admin
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -11,8 +8,5 @@ router.register(r'fruit', views.FruitViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls))
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
